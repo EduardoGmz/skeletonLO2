f889d58d18249ebcd3f983f70eba8eb5b53ce927
Eduardo Gomez <c.eduardo130400@outlook.com>
2022-03-05 20:46:23 -0600

    skeleton

---
+++ sw.js
@@ -0,0 +1,59 @@
+var cacheName = 'weatherPWA-v1';
+var filesToCache = [
+    '/',
+    '/index.html',
+    '/scripts/app.js',
+    '/scripts/localforage-1.4.0.js',
+    '/styles/ud811.css',
+    '/images/clear.png',
+    '/images/cloudy-scattered-showers.png',
+    '/images/cloudy.png',
+    '/images/fog.png',
+    '/images/ic_add_white_24px.svg',
+    '/images/ic_refresh_white_24px.svg',
+    '/images/partly-cloudy.png',
+    '/images/rain.png',
+    '/images/scattered-showers.png',
+    '/images/sleet.png',
+    '/images/snow.png',
+    '/images/thunderstorm.png',
+    '/images/wind.png'
+];
+
+self.addEventListener('install', function(e) {
+    /*Se instala la funcion del service worker y se guarda el app shell
+    en el chache para que la app se pueda usar sin conexion despues*/
+    console.log('[ServiceWorker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+self.addEventListener('activate',function(e){
+    /*Activa la funcion para buscar un cache viejo y eliminarlo*/
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function(keyList){
+            return Promise.all(keyList.map(function(key){
+                if( key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+});
+
+/*Sirve para cuando realizamos una peticion en linea, 
+y tambien podemos manejar esta peticion manualmente*/
+self.addEventListener('fetch', function(e){
+    console.log('[ServiceWorker] fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return.response || fetch(e.request);
+        })
+    );
+});
